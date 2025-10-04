05c9cf64e4f3aa6e27c6fce8019c68afea62d5bf
Twinki <Twinki14@users.noreply.github.com>
2025-10-03 22:49:11 -0400

    tweak

--- debian-init.sh
+++ debian-init.sh
@@ -5,7 +5,7 @@ apt update
 apt install -y curl htop qemu-guest-agent nano git iftop iotop net-tools speedtest-cli iperf3
 
 # Configure tls hostname cert
-read -p "Enter TLS SAN (IP or hostname, eg k3s.den): " TLS_SAN
+read -p "Enter TLS SAN (IP or hostname, eg k3s.den): " TLS_SAN </dev/tty
 
 # Init /etc/rancher/k3s/config.yaml from source
 mkdir -p /etc/rancher/k3s
