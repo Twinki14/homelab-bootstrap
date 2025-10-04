9ff23a7fd309af48446ba8f2e18cb64d2c5078ef
Twinki <Twinki14@users.noreply.github.com>
2025-10-03 22:54:54 -0400

    test

--- debian-init.sh
+++ debian-init.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# Tools
-#apt update
-#apt install -y curl htop qemu-guest-agent nano git iftop iotop net-tools speedtest-cli iperf3
-
 # Configure tls hostname cert
 read -p "Enter TLS SAN (IP or hostname, eg k3s.den): " TLS_SAN
