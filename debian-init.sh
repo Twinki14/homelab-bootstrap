7c82f65476ed33e960b1b2b79ab8927004851b6a
Twinki <Twinki14@users.noreply.github.com>
2025-10-03 22:58:02 -0400

    fix

--- debian-init.sh
+++ debian-init.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
+# Tools
+apt update
+apt install -y curl htop qemu-guest-agent nano git iftop iotop net-tools speedtest-cli iperf3
+
 # Configure tls hostname cert
 read -p "Enter TLS SAN (IP or hostname, eg k3s.den): " TLS_SAN
+
+# Init /etc/rancher/k3s/config.yaml from source
+mkdir -p /etc/rancher/k3s
+wget https://raw.githubusercontent.com/Twinki14/homelab-bootstrap/main/k3s/config.yaml --force-directories -O /etc/rancher/k3s/config.yaml
+sed -i "s|<hostname>|$TLS_SAN|g" /etc/rancher/k3s/config.yaml
+
+# Install k3s
+curl -sfL https://get.k3s.io | sh
+
+# Install flux cli
+curl -sfL https://fluxcd.io/install.sh | bash
+
+# Reboot
+reboot
