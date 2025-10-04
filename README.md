0cd359cea9458ca5ad1ea7f5081ce14933832673
Twinki <Twinki14@users.noreply.github.com>
2025-10-03 22:25:46 -0400

    init

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# homelab-bootstrap
+# Bootstrapping
+
+## k3s node on Debian
+- `wget https://raw.githubusercontent.com/CozyDen/k3s-bootstrap/main/debian-init.sh`
+- `sudo sh ./init.sh`
+
+## Flux
+```
+export GITHUB_TOKEN=<github pat with repo scope>
+```
+
+```bash
+flux bootstrap github \
+  --components-extra=image-reflector-controller,image-automation-controller \
+  --token-auth \
+  --owner=Twinki14 \
+  --repository=CozyLab \
+  --branch=main \
+  --path=flux/ \
+  --personal
+```
---
+++ debian-init.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+apt update
+apt install htop -y
+apt install qemu-guest-agent -y
+apt install nano -y
+apt install git -y
+apt install iftop -y # network bandwidth
+apt install iotop -y # disk i/o
+apt install net-tools -y
+apt install speedtest-cli -y
+DEBIAN_FRONTEND=noninteractive apt install iperf3 -y
+
+# Configure tls hostname cert
+read -p "Enter TLS SAN (IP or hostname, eg k3s.den): " TLS_SAN
+
+# Init /etc/rancher/k3s/config.yaml from source
+wget https://raw.githubusercontent.com/Twinki14/homelab-bootstrap/main/k3s/config.yaml --force-directories -O /etc/rancher/k3s/config.yaml
+sed -i "s|<hostname>|$TLS_SAN|g" /etc/rancher/k3s/config.yaml
+
+# Install k3s
+curl -sfL https://get.k3s.io | sh
+
+# Install flux cli
+curl -s https://fluxcd.io/install.sh | sudo bash
+
+# Reboot
+reboot
